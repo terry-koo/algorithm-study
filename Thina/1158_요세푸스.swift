4fc16a1fb21530107404b85f48360858d9170372
Jungin Yoo <frog7134@gmail.com>
2022-05-01 04:02:13 +0900

    [BOJ] 1158 / 요세푸스 문제 / 16ms
    numArray에 for문을 이용해 1 ~ n값을 채워준다.
    
    numArray의 크기가 index(input[1]은 remove 간격(?)이고 index는 0부터 시작이므로 -1을 추가)보다 클 경우에는 result에 index에 해당하는 numArray 요소를 넣어준다.
    
    index를 간격만큼 이동시킨다.
    
    만약 numArray의 크기가 index보다 큰 경우 다시 처음으로 돌아가야 하므로 나눈 나머지만큼 인덱스가 이동하도록 index %= numArray.count를 쓴다.

--- Thina/1158_요세푸스.swift
+++ Thina/1158_요세푸스.swift
@@ -1,1 +1,25 @@
+import Foundation
 
+let input = readLine()!.components(separatedBy: " ").map { Int($0)! }
+var numArray: [Int] = []
+var result: [Int] = []  // pop한 값을 받을 결과array
+var index: Int = input[1] - 1
+
+for i in 1...input[0] {
+    numArray.append(i)
+}
+
+for _ in numArray {
+    if numArray.count > index {
+        result.append(numArray.remove(at: index))
+        index += input[1] - 1
+        
+    } else {
+        index %= numArray.count
+        result.append(numArray.remove(at: index))
+        index += input[1] - 1
+        
+    }
+}
+
+print("<" + result.map({String($0)}).joined(separator: ", ") + ">")
